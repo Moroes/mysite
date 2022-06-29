c2f2330d07aedf67b0987652d58c7017148f59a3
Moroes <mihaclaim@gmail.com>
2022-06-29 10:38:25 +0300

    initail commit

---
+++ movies/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+
+from .models import Movie
+
+
+class MoviesView(View):
+    """Список фильмов"""
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movie_list.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    """Полное описание фильма"""
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        return render(request, 'movies/movie_detail/.html', {"movie": movie})
