244379d20a43ef4341eb54cf6151635551074945
Moroes <mihaclaim@gmail.com>
2022-09-21 21:52:33 +0300

    изменение настроек статики

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,11 +40,11 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-STATIC_ROOT = BASE_DIR / "static"
+# STATIC_ROOT = BASE_DIR / "static"
 
 # AUTH_USER_MODEL = "main.User"
 
