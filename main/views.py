506ce1927bf20834847a07d89a97c8817cbf5d7d
Moroes <mihaclaim@gmail.com>
2022-11-01 22:17:36 +0300

    изменение атрибутов поля slug модели Post

--- main/views.py
+++ main/views.py
@@ -41,7 +41,7 @@ class CRUD_PostView(View):
 
         if request.method == "POST":  # проверяем то что метод именно POST
             form = PostForm(request.POST, request.FILES)
-            print(form)
+            print(request.POST)
             if form.is_valid():
                 new_post = form.save(commit=False)
                 # Добавление имени пользователя
