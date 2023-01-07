1904f3c2d40ead119b174ee6d8e7fc8a45fbde34
Moroes <mihaclaim@gmail.com>
2023-01-08 00:46:33 +0300

    поправки в css

--- main/forms.py
+++ main/forms.py
@@ -10,11 +10,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields= '__all__'
-        widgets = {'autor': forms.HiddenInput(), 'slug': forms.HiddenInput(), 
-        'description': forms.TextInput(attrs={"class":"input"}), 'title': forms.TextInput(attrs={"class":"input"}), 
-        'tags': forms.TextInput(attrs={"class":"input"}), 'tags': LabelWidget()}
-    
-    # tags = TagField(required=False, widget=LabelWidget)
-
-    # tags = forms.ModelChoiceField(queryset=Tag.objects.all())
-
+        widgets = {'autor': forms.HiddenInput(), 'slug': forms.HiddenInput(),
+        'image': forms.FileInput(attrs={"class": "image"}), 
+        'description': forms.TextInput(attrs={"class": "input description"}),
+        'title': forms.TextInput(attrs={"class": "input"}), 
+        'tags': forms.TextInput(attrs={"class": "input"}), 'tags': LabelWidget()}
